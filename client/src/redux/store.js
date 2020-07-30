7694e28b51e60679cc81b19c784a95367271a73e
jaY-S-99 <33809993+jaY-S-99@users.noreply.github.com>
2020-07-30 14:31:56 +0530

    Removed dev loggers

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware,compose } from "redux";
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
-import logger from 'redux-logger';
 
-const middleware = [thunk,logger];
+const middleware = [thunk];
 
 const initialState = {}
 
